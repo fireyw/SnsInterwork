38a8cdd4581a455dbbb12c3490517536d27f3b75
kywsc <fireyw@gmail.com>
2020-04-26 23:24:06 +0900

    naver profile ask

---
+++ README.md
@@ -0,0 +1,3 @@
+## Sns interwork
+* test Data
+s
--- src/main/java/com/heee/web/controller/controller.java
+++ src/main/java/com/heee/web/controller/controller.java
@@ -1,14 +1,19 @@
 package com.heee.web.controller;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by NESOY on 2017-02-04.
@@ -31,9 +36,87 @@ public class controller {
         return "naverCallback";
     }
 
+    @RequestMapping(value = "/naverPersonalInfo.do")
+    @ResponseBody
+    public String naverPersonalInfo(HttpServletRequest request) throws Exception {
+        String token = "AAAANhMg2DbRmPg_8L7-X3w2O6cs4CW9rN93LX6oTmfbN3vRKPpgDNBQiVpwLZiA6w0fedtjfPrU181yRvpkbahDi2M"; // 네이버 로그인 접근 토큰;
+        String header = "Bearer " + token; // Bearer 다음에 공백 추가
+
+        String apiURL = "https://openapi.naver.com/v1/nid/me";
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("Authorization", header);
+        String responseBody = get(apiURL,requestHeaders);
+
+        System.out.println(responseBody);
+        JSONObject jsonObject = new JSONObject(responseBody);
+        String newJSON = jsonObject.getString("message");
+        System.out.println(newJSON.toString());
+
+        return responseBody;
+
+//        JSONArray cars = (JSONArray) jsonObject.get("cars");
+//        Iterator<String> iterator = cars.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+
+
+    }
+
+    private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+
+
     @RequestMapping(value = "/personalInfo.do")
     public void personalInfo(HttpServletRequest request) throws Exception {
-        String token = "AAAANrhDbtNE3s1-IGbG3-YVLKjEYyjey_Bn9OE3PbgzDa8NK5yAj1HzLrM2xm02tdI0LJ6b7QMaH4uqZXZv5LLpFZo";// 네이버 로그인 접근 토큰; 여기에 복사한 토큰값을 넣어줍니다.
+        String token = "AAAANhMg2DbRmPg_8L7-X3w2O6cs4CW9rN93LX6oTmfbN3vRKPpgDNBQiVpwLZiA6w0fedtjfPrU181yRvpkbahDi2M";// 네이버 로그인 접근 토큰; 여기에 복사한 토큰값을 넣어줍니다.
         String header = "Bearer " + token; // Bearer 다음에 공백 추가
         try {
             String apiURL = "https://openapi.naver.com/v1/nid/me";
@@ -44,9 +127,9 @@ public class controller {
             int responseCode = con.getResponseCode();
             BufferedReader br;
             if (responseCode == 200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
             } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
             }
             String inputLine;
             StringBuffer response = new StringBuffer();
@@ -54,7 +137,7 @@ public class controller {
                 response.append(inputLine);
             }
             br.close();
-            System.out.println(response.toString());
+            System.out.println("결과값 : " + response.toString());
         } catch (Exception e) {
             System.out.println(e);
         }
